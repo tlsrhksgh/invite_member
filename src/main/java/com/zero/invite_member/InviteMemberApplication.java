66755e70355dcc1c380e318e0be1f1a0db3f0169
tlsrhksgh <sin99145@naver.com>
2023-06-12 15:16:21 +0900

    Initial commit

---
+++ src/main/java/com/zero/invite_member/InviteMemberApplication.java
@@ -0,0 +1,13 @@
+package com.zero.invite_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InviteMemberApplication.class, args);
+    }
+
+}
